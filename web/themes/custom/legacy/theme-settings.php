01deab0937ff722a8b7370ec52caa33ad4cf36ba
Ayinde Clarke <aclarke@asce.org>
2017-05-24 13:09:28 -0400

    year sections sorted in vertical tabs
    Signed-off-by: Ayinde Clarke <aclarke@asce.org>

--- web/themes/custom/legacy/theme-settings.php
+++ web/themes/custom/legacy/theme-settings.php
@@ -5,13 +5,13 @@ require_once dirname(__FILE__) . '/includes/next_year_settings.php';
 
 function legacy_form_system_theme_settings_alter(&$form, &$form_state){
   $form['legacy_theme_settings'] = [
-    // '#type' => 'vertical_tabs',
+    '#type' => 'vertical_tabs',
     '#parents' => ['legacy_theme_settings'],
   ];
 
   $form['theme_settings']['#group'] = 'legacy_theme_settings';
-  $form['favicon']['#group'] = 'legacy_theme_settings';
   legacy_current_year_settings_form_alter($form);
   // legacy_prev_year_settings_form_alter($form);
   // legacy_next_year_settings_form_alter($form);
+  // $form['favicon']['#group'] = 'legacy_theme_settings';
 }
