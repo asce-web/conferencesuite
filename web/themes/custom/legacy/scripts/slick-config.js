52b5cf7dbfd2e1e68cb4648bb553032c18e546d2
Chris Harvey (work) <charvey@asce.org>
2017-08-14 13:59:48 -0400

    add slick assets

---
+++ web/themes/custom/legacy/scripts/slick-config.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  let $ = jQuery
+  $('.c-SupporterBlock__Slick').slick({
+    dots: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+  })
+})
