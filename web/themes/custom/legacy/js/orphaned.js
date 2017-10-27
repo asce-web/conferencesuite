41466d3e82be87b2f0d9eb18bedb1ea4035ea52b
Chris Harvey (work) <charvey@asce.org>
2017-10-27 14:43:56 -0400

    rename drawer-related components

--- web/themes/custom/legacy/js/orphaned.js
+++ web/themes/custom/legacy/js/orphaned.js
@@ -6,13 +6,3 @@
 
 
 $('.js-xo-_1').addClass('-xo-_1');
-$('.o-Drawer:not(.o-Drawer--open)').find('a').attr('tabindex', -1);
-$('.c-Toggle').click(function () {
-  var drawer = $(this).siblings('.o-Drawer')
-  var is_open = drawer.attr('aria-expanded') === 'true'
-  drawer
-    .toggleClass('o-Drawer--open')
-    .attr('aria-expanded', !is_open)
-    .attr('aria-hidden'  ,  is_open)
-  $(this).find('.c-Toggle__Viz').toggleClass('glyphicons-menu-hamburger glyphicons-remove-circle')
-})
