896babe341c3b14bbaa0ffed280d9a84248c7492
Chris Harvey (work) <charvey@asce.org>
2017-10-27 14:28:12 -0400

    restructure drawer attributes

--- web/themes/custom/legacy/js/orphaned.js
+++ web/themes/custom/legacy/js/orphaned.js
@@ -8,9 +8,11 @@
 $('.js-xo-_1').addClass('-xo-_1');
 $('.o-Drawer:not(.o-Drawer--open)').find('a').attr('tabindex', -1);
 $('.c-Toggle').click(function () {
-  var is_open = ($(this).attr('aria-expanded') === 'true')
-  $(this).attr('aria-expanded', !is_open)
-  $(this).parents('nav').find('.o-Drawer').toggleClass('o-Drawer--open')
-    .find('a').attr('tabindex', (is_open) ? -1 : 0)
+  var drawer = $(this).siblings('.o-Drawer')
+  var is_open = drawer.attr('aria-expanded') === 'true'
+  drawer
+    .toggleClass('o-Drawer--open')
+    .attr('aria-expanded', !is_open)
+    .attr('aria-hidden'  ,  is_open)
   $(this).find('.c-Toggle__Viz').toggleClass('glyphicons-menu-hamburger glyphicons-remove-circle')
 })
