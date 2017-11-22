bf03b4afd3465ac06687674932e682d313d81e89
Chris Harvey <chrisharvey2pi@gmail.com>
2017-11-22 13:41:25 -0500

    uniform menu animations

--- web/themes/custom/legacy/js/o-Drawer.js
+++ web/themes/custom/legacy/js/o-Drawer.js
@@ -1,3 +1,4 @@
+(function ($) {
 $('.o-Drawer__Bar:not(.o-Drawer__Bar--js-open)').find('a').attr('tabindex', -1);
 $('.o-Drawer__Button').click(function () {
   var drawer = $(this).siblings('.o-Drawer__Bar');
@@ -9,3 +10,4 @@ $('.o-Drawer__Button').click(function () {
     .find('a').attr('tabindex', (is_open) ? -1 : 0);
   $(this).find('.glyphicons').toggleClass('glyphicons-menu-hamburger glyphicons-remove-circle');
 });
+})(jQuery)
