3a14fc0c60813fd94c6bde21c7132388ec666543
Chris Harvey (work) <charvey@asce.org>
2019-02-28 10:36:25 -0500

    update gulpfile && add .gitattributes
    new file:   .gitattributes

--- web/themes/custom/legacy/gulpfile.js
+++ web/themes/custom/legacy/gulpfile.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 // require('@babel/core')         // DO NOT REMOVE … required by `gulp-babel`
-// require('babel-preset-env')    // DO NOT REMOVE … required by babel preset configs
+// require('@babel/preset-env')   // DO NOT REMOVE … required by babel preset configs
 // require('babel-preset-minify') // DO NOT REMOVE … required by babel preset configs
 const kss          = require('kss')
 const gulp         = require('gulp')
@@ -12,33 +12,7 @@ const autoprefixer = require('gulp-autoprefixer')
 const clean_css    = require('gulp-clean-css')
 const sourcemaps   = require('gulp-sourcemaps')
 
-gulp.task('test', async function () {
-  return gulp.src(__dirname + '/proto/legacytest.pug')
-    .pipe(pug({
-      basedir: './',
-      locals: {
-      }
-    }))
-    .pipe(gulp.dest('./proto/'))
-})
-
-// HOW-TO: https://github.com/kss-node/kss-node/issues/161#issuecomment-222292620
-gulp.task('docs-kss-markup', async function () {
-  return kss(require('./config/kss.json'))
-})
-
-gulp.task('docs-kss-style', async function () {
-  return gulp.src('./docs/css/kss-custom.less')
-  .pipe(less())
-  .pipe(autoprefixer({
-    grid: true,
-  }))
-  .pipe(gulp.dest('./docs/styleguide/'))
-})
-
-gulp.task('docs', ['docs-kss-markup', 'docs-kss-style'])
-
-gulp.task('dist-style', async function () {
+function dist_style() {
   return gulp.src(['./css/src/*.less', '!./css/src/__*.less'])
     .pipe(sourcemaps.init())
     .pipe(less())
@@ -56,18 +30,58 @@ gulp.task('dist-style', async function () {
     }))
     .pipe(sourcemaps.write('./')) // writes to an external .map file
     .pipe(gulp.dest('./css/dist/'))
-})
+}
 
-gulp.task('dist-script', async function () {
+function dist_script() {
   return gulp.src('./js/src/*.js')
     .pipe(sourcemaps.init())
     .pipe(babel({
-      presets: ['env', 'minify']
+			presets: [
+				'@babel/preset-env',
+				'minify',
+			]
     }))
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./js/dist/'))
-})
+}
 
-gulp.task('dist', ['dist-style', 'dist-script'])
+const dist = gulp.parallel(dist_style, dist_script)
 
-gulp.task('build', ['test', 'docs', 'dist'])
+function test() {
+  return gulp.src(__dirname + '/proto/legacytest.pug')
+    .pipe(pug({
+      basedir: './',
+      locals: {
+      }
+    }))
+    .pipe(gulp.dest('./proto/'))
+}
+
+// HOW-TO: https://github.com/kss-node/kss-node/issues/161#issuecomment-222292620
+async function docs_kss_markup() {
+  return kss(require('./config/kss.json'))
+}
+
+function docs_kss_style() {
+  return gulp.src('./docs/css/kss-custom.less')
+  .pipe(less())
+  .pipe(autoprefixer({
+    grid: true,
+  }))
+  .pipe(gulp.dest('./docs/styleguide/'))
+}
+
+const docs = gulp.parallel(docs_kss_markup, docs_kss_style)
+
+const build = gulp.parallel(dist, test, docs)
+
+module.exports = {
+	build,
+		dist,
+			dist_style,
+			dist_script,
+		test,
+		docs,
+			docs_kss_markup,
+			docs_kss_style,
+}
