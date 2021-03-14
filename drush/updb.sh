3edff4d5f994d8e8f09c95f3f04f186b7c94b8ce
Hari Prasad Mohan <hmohan@asce.org>
2021-03-14 19:55:20 -0400

    added 2022 structures congress & update db script

---
+++ drush/updb.sh
@@ -0,0 +1,137 @@
+#!/bin/sh
+#https://www.aei-conference.org
+drush @confsuite.aeiconf updb
+drush @confsuite.aeiconf_19 updb
+drush @confsuite.aeiconf_21 updb
+#https://www.aei-forum.org
+drush @confsuite.aei_forum updb
+drush @confsuite.aei_forum_20 updb
+#https://www.aeippa.org
+drush @confsuite.aeippa updb
+drush @confsuite.aeippa_20 updb
+#https://www.aeisdc.org
+drush @confsuite.aeisdc updb
+drush @confsuite.aeisdc_19 updb
+drush @confsuite.aeisdc_20 updb
+drush @confsuite.aeisdc_21 updb
+#https://www.apmconference.org
+drush @confsuite.apm updb
+drush @confsuite.apm_20 updb
+#https://www.asce-ictd.org
+drush @confsuite.asce_ictd updb
+drush @confsuite.asce_ictd_18 updb
+drush @confsuite.asce_ictd_19 updb
+drush @confsuite.asce_ictd_20 updb
+#https://www.asceconvention.org
+drush @confsuite.asceconv updb
+drush @confsuite.asceconv_18 updb
+drush @confsuite.asceconv_19 updb
+drush @confsuite.asceconv_20 updb
+#https://www.ascestudentdays.org
+drush @confsuite.ascestudentdays updb
+drush @confsuite.ascestudentdays_18 updb
+drush @confsuite.ascestudentdays_19 updb
+drush @confsuite.ascestudentdays_20 updb
+#https://www.asceweek.org
+drush @confsuite.asceweek updb
+drush @confsuite.asceweek_18 updb
+#https://www.asceweekinternational.org
+drush @confsuite.asceweekint updb
+drush @confsuite.asceweekint_19 updb
+#https://www.cisummit.org
+drush @confsuite.cisummit updb
+drush @confsuite.cisummit_18 updb
+drush @confsuite.cisummit_19 updb
+drush @confsuite.cisummit_20 updb
+drush @confsuite.cisummit_21 updb
+#https://www.cisummit-crc.asce.org
+drush @confsuite.cisummit-crc updb
+#https://www.earthspaceconference.org
+drush @confsuite.esconf updb
+drush @confsuite.esconf_20 updb
+#https://www.emi-conference.org
+drush @confsuite.emi updb
+drush @confsuite.emi_20 updb
+#https://www.etsconference.org
+drush @confsuite.ets updb
+drush @confsuite.ets_18 updb
+#https://www.ewricongress.org
+drush @confsuite.ewri updb
+drush @confsuite.ewri_18 updb
+drush @confsuite.ewri_19 updb
+drush @confsuite.ewri_20 updb
+drush @confsuite.ewri_21 updb
+#https://www.forensiccongress.org
+drush @confsuite.forensic updb
+drush @confsuite.forensic_18 updb
+drush @confsuite.forensic_21 updb
+#https://www.geo-extreme.org
+drush @confsuite.geo-extreme updb
+drush @confsuite.geo-extreme_21 updb
+#https://www.geocongress.org
+drush @confsuite.geo updb
+drush @confsuite.geo_19 updb
+drush @confsuite.geo_20 updb
+#https://www.geesdconference.org
+drush @confsuite.geesd updb
+drush @confsuite.geesd_18 updb
+#https://www.icce2018.com
+drush @confsuite.icce updb
+drush @confsuite.icce_18 updb
+#https://www.2020icse.org
+drush @confsuite.icse updb
+drush @confsuite.icse_20 updb
+#https://www.icsiconference.org
+drush @confsuite.icsi updb
+drush @confsuite.icsi_19 updb
+drush @confsuite.icsi_21 updb
+#https://www.isfog2020.org
+drush @confsuite.isfog updb
+drush @confsuite.isfog_20 updb
+#https://www.lidconference.org
+drush @confsuite.lid updb
+drush @confsuite.lid_18 updb
+drush @confsuite.lid_20 updb
+#https://www.omswconference.org
+drush @confsuite.omsw updb
+drush @confsuite.omsw_19 updb
+drush @confsuite.omsw_21 updb
+#https://www.pavementsconference.org
+drush @confsuite.pavements updb
+drush @confsuite.pavements_19 updb
+#https://www.pipelinesconference.org
+drush @confsuite.pipe updb
+drush @confsuite.pipe_18 updb
+drush @confsuite.pipe_19 updb
+drush @confsuite.pipe_20 updb
+drush @confsuite.pipe_21 updb
+#https://www.portsconference.org
+drush @confsuite.ports updb
+drush @confsuite.ports_19 updb
+#https://www.structurescongress.org
+drush @confsuite.structures updb
+drush @confsuite.structures_19 updb
+drush @confsuite.structures_20 updb
+drush @confsuite.structures_21 updb
+drush @confsuite.structures_22 updb
+#https://www.surveyingconference.org
+drush @confsuite.surveying updb
+drush @confsuite.surveying_18 updb
+drush @confsuite.surveying_20 updb
+#https://www.tdileadership.org
+drush @confsuite.tdi updb
+drush @confsuite.tdi_20 updb
+#https://www.udmconference2021.org
+drush @confsuite.udmconf updb
+drush @confsuite.udmconf_21 updb
+#https://www.watershedmanagementconference.org
+drush @confsuite.watershed updb
+drush @confsuite.watershed_20 updb
+#https://canary.conferencesuite.asce.org
+drush @confsuite.canary updb
+#https://conferencesuite.asce.org
+drush @confsuite.confsuite updb
+#https://dev.conferencesuite.asce.org
+drush @confsuite.dev updb
+#https://training.conferencesuite.asce.org
+drush @confsuite.training updb
